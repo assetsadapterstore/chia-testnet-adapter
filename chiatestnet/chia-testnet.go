2b0232a9d94bdcabf8f7045af5426942fbbe3ab5
ldx <kencani2@163.com>
2021-05-07 22:00:16 +0800

    chia for testnet

---
+++ chiatestnet/chia-testnet.go
@@ -0,0 +1,28 @@
+package chiatestnet
+
+
+import (
+	"github.com/assetsadapterstore/chia-adapter/chia"
+	"github.com/blocktree/openwallet/v2/log"
+)
+
+const (
+	Symbol = "TXCH"
+)
+
+type WalletManager struct {
+	*chia.WalletManager
+}
+
+func NewWalletManager() *WalletManager {
+	wm := WalletManager{}
+	wm.WalletManager = chia.NewWalletManager()
+	wm.Config = chia.NewConfig(Symbol)
+	wm.Log = log.NewOWLogger(wm.Symbol())
+	return &wm
+}
+
+//FullName 币种全名
+func (wm *WalletManager) FullName() string {
+	return "CHIA-TESTNET"
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/assetsadapterstore/chia-testnet-adapter
+
+go 1.12
+
+require (
+	github.com/assetsadapterstore/chia-adapter v1.0.1
+	github.com/astaxie/beego v1.12.1
+	github.com/blocktree/openwallet/v2 v2.3.0
+)
